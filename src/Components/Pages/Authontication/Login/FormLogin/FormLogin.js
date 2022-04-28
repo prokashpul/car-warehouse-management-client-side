94d35e4a685b463ef0e635ef8c18a9794a60688b
prokash <pprokash102@gmail.com>
2022-04-28 23:51:59 +0600

    webcarmanager react app create and set up reactrouter and firebase hosting

---
+++ src/Components/Pages/Authontication/Login/FormLogin/FormLogin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FOrmLogin = () => {
+  return <div>Form Login</div>;
+};
+
+export default FOrmLogin;
---
+++ src/Components/Pages/Authontication/Registration/Registration.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ragistration = () => {
+  return <div>registration</div>;
+};
+
+export default Ragistration;
